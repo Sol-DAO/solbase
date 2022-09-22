47db0f51c13e6bcb93ae5d17f5781be7c0062833
z0r0z <zrzr8963@gmail.com>
2022-09-22 12:34:38 +0000

    add sep mock file for supply check

--- src/test/utils/mocks/MockERC721Supply.sol
+++ src/test/utils/mocks/MockERC721Supply.sol
@@ -3,21 +3,35 @@ pragma solidity ^0.8.0;
 
 import {ERC721} from "../../../tokens/ERC721.sol";
 
-contract MockERC721 is ERC721 {
+contract MockERC721Supply is ERC721 {
+    uint256 public totalSupply;
+
     constructor(string memory _name, string memory _symbol) ERC721(_name, _symbol) {}
 
     function tokenURI(uint256) public pure virtual override returns (string memory) {}
 
     function mint(address to, uint256 tokenId) public virtual {
         _mint(to, tokenId);
+
+        unchecked {
+            ++totalSupply;
+        }
     }
 
     function burn(uint256 tokenId) public virtual {
         _burn(tokenId);
+
+        unchecked {
+            --totalSupply;
+        }
     }
 
     function safeMint(address to, uint256 tokenId) public virtual {
         _safeMint(to, tokenId);
+
+        unchecked {
+            ++totalSupply;
+        }
     }
 
     function safeMint(
@@ -26,5 +40,9 @@ contract MockERC721 is ERC721 {
         bytes memory data
     ) public virtual {
         _safeMint(to, tokenId, data);
+
+        unchecked {
+            ++totalSupply;
+        }
     }
 }
