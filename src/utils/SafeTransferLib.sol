7dc6fe162edb730c092ec7179cce0d7b9b400174
z0r0z <92001561+z0r0z@users.noreply.github.com>
2022-09-22 16:42:00 +0900

    Update SafeTransferLib.sol
    ✍️ header style fix and logical reordering

--- src/utils/SafeTransferLib.sol
+++ src/utils/SafeTransferLib.sol
@@ -6,21 +6,21 @@ pragma solidity ^0.8.4;
 /// @author Modified from Solady (https://github.com/vectorized/solady/blob/main/src/utils/SafeTransferLib.sol)
 /// @dev Caution! This library won't check that a token has code, responsibility is delegated to the caller.
 library SafeTransferLib {
-    /*//////////////////////////////////////////////////////////////
-                              CUSTOM ERRORS
-    //////////////////////////////////////////////////////////////*/
+    /// -----------------------------------------------------------------------
+    /// Custom Errors 
+    /// -----------------------------------------------------------------------
 
     error ETHTransferFailed();
+    
+    error ApproveFailed();
+    
+    error TransferFailed();
 
     error TransferFromFailed();
 
-    error TransferFailed();
-
-    error ApproveFailed();
-
-    /*//////////////////////////////////////////////////////////////
-                             ETH OPERATIONS                       
-    //////////////////////////////////////////////////////////////*/
+    /// -----------------------------------------------------------------------
+    /// ETH Operations 
+    /// -----------------------------------------------------------------------
 
     function safeTransferETH(address to, uint256 amount) internal {
         assembly {
@@ -34,13 +34,12 @@ library SafeTransferLib {
         }
     }
 
-    /*//////////////////////////////////////////////////////////////
-                            ERC20 OPERATIONS                      
-    //////////////////////////////////////////////////////////////*/
-
-    function safeTransferFrom(
+    /// -----------------------------------------------------------------------
+    /// ERC20 Operations 
+    /// -----------------------------------------------------------------------
+    
+    function safeApprove(
         address token,
-        address from,
         address to,
         uint256 amount
     ) internal {
@@ -49,33 +48,31 @@ library SafeTransferLib {
             let memPointer := mload(0x40)
 
             // Write the abi-encoded calldata into memory, beginning with the function selector.
-            mstore(0x00, 0x23b872dd)
-            mstore(0x20, from) // Append the "from" argument.
-            mstore(0x40, to) // Append the "to" argument.
-            mstore(0x60, amount) // Append the "amount" argument.
+            mstore(0x00, 0x095ea7b3)
+            mstore(0x20, to) // Append the "to" argument.
+            mstore(0x40, amount) // Append the "amount" argument.
 
             if iszero(
                 and(
                     // Set success to whether the call reverted, if not we check it either
                     // returned exactly 1 (can't just be non-zero data), or had no return data.
                     or(eq(mload(0x00), 1), iszero(returndatasize())),
-                    // We use 0x64 because that's the total length of our calldata (0x04 + 0x20 * 3)
+                    // We use 0x44 because that's the total length of our calldata (0x04 + 0x20 * 2)
                     // Counterintuitively, this call() must be positioned after the or() in the
                     // surrounding and() because and() evaluates its arguments from right to left.
-                    call(gas(), token, 0, 0x1c, 0x64, 0x00, 0x20)
+                    call(gas(), token, 0, 0x1c, 0x44, 0x00, 0x20)
                 )
             ) {
-                // Store the function selector of `TransferFromFailed()`.
-                mstore(0x00, 0x7939f424)
+                // Store the function selector of `ApproveFailed()`.
+                mstore(0x00, 0x3e3f8f73)
                 // Revert with (offset, size).
                 revert(0x1c, 0x04)
             }
 
-            mstore(0x60, 0) // Restore the zero slot to zero.
             mstore(0x40, memPointer) // Restore the memPointer.
         }
     }
-
+    
     function safeTransfer(
         address token,
         address to,
@@ -111,8 +108,9 @@ library SafeTransferLib {
         }
     }
 
-    function safeApprove(
+    function safeTransferFrom(
         address token,
+        address from,
         address to,
         uint256 amount
     ) internal {
@@ -121,27 +119,29 @@ library SafeTransferLib {
             let memPointer := mload(0x40)
 
             // Write the abi-encoded calldata into memory, beginning with the function selector.
-            mstore(0x00, 0x095ea7b3)
-            mstore(0x20, to) // Append the "to" argument.
-            mstore(0x40, amount) // Append the "amount" argument.
+            mstore(0x00, 0x23b872dd)
+            mstore(0x20, from) // Append the "from" argument.
+            mstore(0x40, to) // Append the "to" argument.
+            mstore(0x60, amount) // Append the "amount" argument.
 
             if iszero(
                 and(
                     // Set success to whether the call reverted, if not we check it either
                     // returned exactly 1 (can't just be non-zero data), or had no return data.
                     or(eq(mload(0x00), 1), iszero(returndatasize())),
-                    // We use 0x44 because that's the total length of our calldata (0x04 + 0x20 * 2)
+                    // We use 0x64 because that's the total length of our calldata (0x04 + 0x20 * 3)
                     // Counterintuitively, this call() must be positioned after the or() in the
                     // surrounding and() because and() evaluates its arguments from right to left.
-                    call(gas(), token, 0, 0x1c, 0x44, 0x00, 0x20)
+                    call(gas(), token, 0, 0x1c, 0x64, 0x00, 0x20)
                 )
             ) {
-                // Store the function selector of `ApproveFailed()`.
-                mstore(0x00, 0x3e3f8f73)
+                // Store the function selector of `TransferFromFailed()`.
+                mstore(0x00, 0x7939f424)
                 // Revert with (offset, size).
                 revert(0x1c, 0x04)
             }
 
+            mstore(0x60, 0) // Restore the zero slot to zero.
             mstore(0x40, memPointer) // Restore the memPointer.
         }
     }
