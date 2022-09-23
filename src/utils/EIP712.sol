2a2b783d550ddd6203596aa4732cf78397f92c63
0xClandestine <96172957+0xClandestine@users.noreply.github.com>
2022-09-23 04:11:09 -0500

    🏠 chore: initial draft EIP712.sol

---
+++ src/utils/EIP712.sol
@@ -0,0 +1,54 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+abstract contract EIP712 {
+
+    bytes32 internal constant DOMAIN_TYPEHASH = keccak256("EIP712Domain(string name,string version,uint256 chainId,address verifyingContract)");
+    
+    bytes32 internal immutable HASHED_DOMAIN_NAME;
+
+    bytes32 internal immutable HASHED_DOMAIN_VERSION;
+    
+    bytes32 internal immutable INITIAL_DOMAIN_SEPARATOR;
+
+    uint256 internal immutable INITIAL_CHAIN_ID;
+
+    constructor(string memory domainName, string memory version) {
+        
+        HASHED_DOMAIN_NAME = keccak256(bytes(domainName));
+
+        HASHED_DOMAIN_VERSION = keccak256(bytes(version));
+        
+        INITIAL_DOMAIN_SEPARATOR = computeDomainSeparator();
+
+        INITIAL_CHAIN_ID = block.chainid;
+    }
+
+    function DOMAIN_SEPARATOR() public view virtual returns (bytes32) {
+        return block.chainid == INITIAL_CHAIN_ID ? INITIAL_DOMAIN_SEPARATOR : computeDomainSeparator();
+    }
+
+    function computeDomainSeparator() internal view virtual returns (bytes32) {
+        return
+            keccak256(
+                abi.encode(
+                    DOMAIN_TYPEHASH,
+                    HASHED_DOMAIN_NAME,
+                    HASHED_DOMAIN_VERSION,
+                    block.chainid,
+                    address(this)
+                )
+            );
+    }
+
+    function computeDigest(bytes32 hashStruct) internal view returns (bytes32) {
+        return
+            keccak256(
+                abi.encodePacked(
+                    "\x19\x01", 
+                    DOMAIN_SEPARATOR(), 
+                    hashStruct
+                )
+            );
+    }
+}
