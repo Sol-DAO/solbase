7e89658def183e70d15dca5368e664d19c14ee52
z0r0z <zrzr8963@gmail.com>
2022-09-23 12:53:41 +0000

    cleanup

--- src/utils/EIP712.sol
+++ src/utils/EIP712.sol
@@ -4,6 +4,10 @@ pragma solidity ^0.8.4;
 /// @notice Gas-optimized implementation of EIP-712 domain separator and digest encoding.
 /// @author SolDAO (https://github.com/Sol-DAO/solbase/blob/main/src/utils/EIP712.sol)
 abstract contract EIP712 {
+    /// -----------------------------------------------------------------------
+    /// Domain Constants
+    /// -----------------------------------------------------------------------
+
     /// @dev `keccak256("EIP712Domain(string name,string version,uint256 chainId,address verifyingContract)")`.
     bytes32 internal constant DOMAIN_TYPEHASH = 0x8b73c3c69bb8fe3d512ecc4cf759cc79239f7b179b0ffacaa9a75d522b39400f;
 
@@ -28,6 +32,10 @@ abstract contract EIP712 {
 
         INITIAL_CHAIN_ID = block.chainid;
     }
+
+    /// -----------------------------------------------------------------------
+    /// EIP-712 Logic
+    /// -----------------------------------------------------------------------
 
     function DOMAIN_SEPARATOR() public view virtual returns (bytes32) {
         return block.chainid == INITIAL_CHAIN_ID ? INITIAL_DOMAIN_SEPARATOR : computeDomainSeparator();
