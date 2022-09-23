0146cc1851eb075cfedec3ec386aef759e4cc154
0xClandestine <96172957+0xClandestine@users.noreply.github.com>
2022-09-23 18:42:27 -0500

    🏠 chore: fix imports

--- src/test/utils/mocks/MockERC721.sol
+++ src/test/utils/mocks/MockERC721.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
 
-import {ERC721} from "../../../tokens/ERC721.sol";
+import {ERC721} from "../../../tokens/ERC721/ERC721.sol";
 
 contract MockERC721 is ERC721 {
     constructor(string memory _name, string memory _symbol) ERC721(_name, _symbol) {}
