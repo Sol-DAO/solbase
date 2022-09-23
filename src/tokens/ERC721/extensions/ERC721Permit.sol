a94be32b564dec1338d6736510641c176f3909fd
0xClandestine <96172957+0xClandestine@users.noreply.github.com>
2022-09-23 16:38:30 -0500

    🏠 chore: ERC721Permit draft

---
+++ src/tokens/ERC721/extensions/ERC721Permit.sol
@@ -0,0 +1,70 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+import {ERC721} from "../../../tokens/ERC721/ERC721.sol";
+import {EIP712} from "../../../utils/EIP712.sol";
+
+abstract contract ERC722Permit is ERC721, EIP712 {
+
+    /// -----------------------------------------------------------------------
+    /// EIP-2612-Style Storage
+    /// -----------------------------------------------------------------------
+
+    mapping(uint256 => uint256) public nonces;
+
+    /// -----------------------------------------------------------------------
+    /// Constructor
+    /// -----------------------------------------------------------------------
+
+    constructor(
+        string memory _name, 
+        string memory _symbol
+    ) ERC721(_name, _symbol) EIP712(_name, "1") {}
+
+    /// -----------------------------------------------------------------------
+    /// EIP-2612-Style Permit Logic
+    /// -----------------------------------------------------------------------
+
+    function permit(
+        address spender,
+        uint256 id,
+        uint256 deadline,
+        uint8 v,
+        bytes32 r,
+        bytes32 s
+    ) public virtual {
+
+        require(spender != address(0), "INVALID_SPENDER");
+
+        require(deadline >= block.timestamp, "PERMIT_DEADLINE_EXPIRED");
+
+        // Unchecked because the only math done is incrementing
+        // the owner's nonce which cannot realistically overflow.
+        unchecked {
+            address recoveredAddress = ecrecover(
+                computeDigest(
+                    keccak256(
+                        abi.encode(
+                            keccak256(
+                                "Permit(address spender,uint256 id,uint256 nonce,uint256 deadline)"
+                            ),
+                            spender,
+                            id,
+                            nonces[id]++,
+                            deadline
+                        )
+                    )
+                ),
+                v,
+                r,
+                s
+            );
+
+            require(recoveredAddress != address(0) && recoveredAddress == _ownerOf[id], "INVALID_SIGNER");
+
+            getApproved[id] = spender;
+
+            emit Approval(recoveredAddress, spender, id);
+        }
+    }
+}
