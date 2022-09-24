0b9064b455a4cf8e2c8057b1e83fe969ee7ba746
0xClandestine <96172957+0xClandestine@users.noreply.github.com>
2022-09-24 00:11:27 -0500

    🏠 chore: draft initial ERC1155Permit && tests
    missing fuzz tests as well

---
+++ src/tokens/ERC1155/extensions/ERC1155Permit.sol
@@ -0,0 +1,71 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+import {ERC1155} from "../../../tokens/ERC1155/ERC1155.sol";
+import {EIP712} from "../../../utils/EIP712.sol";
+
+abstract contract ERC1155Permit is ERC1155, EIP712 {
+
+    /// -----------------------------------------------------------------------
+    /// EIP-2612-Style Storage
+    /// -----------------------------------------------------------------------
+
+    mapping(address => mapping(uint256 => uint256)) public nonces;
+
+    /// -----------------------------------------------------------------------
+    /// Constructor
+    /// -----------------------------------------------------------------------
+
+    constructor(string memory _name) EIP712(_name, "1") {}
+
+    /// -----------------------------------------------------------------------
+    /// EIP-2612-Style Permit Logic
+    /// -----------------------------------------------------------------------
+
+    function permit(
+        address owner,
+        address spender,
+        uint256 id,
+        uint256 deadline,
+        uint8 v,
+        bytes32 r,
+        bytes32 s
+    ) public virtual {
+
+        require(spender != address(0), "INVALID_SPENDER");
+        
+        require(deadline >= block.timestamp, "PERMIT_DEADLINE_EXPIRED");
+
+        // Unchecked because the only math done is incrementing
+        // the owner's nonce which cannot realistically overflow.
+        unchecked {
+            address recoveredAddress = ecrecover(
+                computeDigest(
+                    keccak256(
+                        abi.encode(
+                            keccak256(
+                                "Permit(address owner,address spender,uint256 id,uint256 nonce,uint256 deadline)"
+                            ),
+                            spender,
+                            id,
+                            nonces[owner][id]++,
+                            deadline
+                        )
+                    )
+                ),
+                v,
+                r,
+                s
+            );
+
+            require(
+                recoveredAddress == owner && recoveredAddress != address(0),
+                "INVALID SIGNER"
+            );
+
+            isApprovedForAll[owner][spender] = true;
+
+            emit ApprovalForAll(owner, spender, true); 
+        }
+    }
+}
