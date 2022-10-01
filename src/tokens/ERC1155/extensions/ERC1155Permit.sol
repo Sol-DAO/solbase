28b0690bfc38f58442323a5896652e38b1531686
z0r0z <zrzr8963@gmail.com>
2022-10-01 12:49:15 +0900

    🥢Cleanups and Comment fixes

--- src/tokens/ERC1155/extensions/ERC1155Permit.sol
+++ src/tokens/ERC1155/extensions/ERC1155Permit.sol
@@ -4,19 +4,18 @@ pragma solidity ^0.8.4;
 import {ERC1155} from "../../../tokens/ERC1155/ERC1155.sol";
 import {EIP712} from "../../../utils/EIP712.sol";
 
-/// @notice ERC1155 + EIP-2612-Style implementation.
+/// @notice ERC1155 + EIP-2612-style implementation.
 /// @author SolDAO (https://github.com/Sol-DAO/solbase/blob/main/src/tokens/ERC1155/extensions/ERC1155Permit.sol)
 abstract contract ERC1155Permit is ERC1155, EIP712 {
-    
     /// -----------------------------------------------------------------------
-    /// EIP-2612-Style Constants
+    /// EIP-2612-style Constants
     /// -----------------------------------------------------------------------
 
-    // @dev 'keccak256("Permit(address owner,address spender,uint256 id,uint256 nonce,uint256 deadline)")'
+    /// @dev `keccak256("Permit(address owner,address spender,uint256 id,uint256 nonce,uint256 deadline)")`.
     bytes32 public constant PERMIT_TYPEHASH = 0x29da74a9365f97c3d77de334aec5c720e44b0c8a6e640ceb375e27a8ab7acadd;
-    
+
     /// -----------------------------------------------------------------------
-    /// EIP-2612-Style Storage
+    /// EIP-2612-style Storage
     /// -----------------------------------------------------------------------
 
     mapping(address => mapping(uint256 => uint256)) public nonces;
@@ -28,7 +27,7 @@ abstract contract ERC1155Permit is ERC1155, EIP712 {
     constructor(string memory domainName) EIP712(domainName, "1") {}
 
     /// -----------------------------------------------------------------------
-    /// EIP-2612-Style Permit Logic
+    /// EIP-2612-style Permit Logic
     /// -----------------------------------------------------------------------
 
     function permit(
@@ -40,7 +39,6 @@ abstract contract ERC1155Permit is ERC1155, EIP712 {
         bytes32 r,
         bytes32 s
     ) public virtual {
-
         require(deadline >= block.timestamp, "PERMIT_DEADLINE_EXPIRED");
 
         // Unchecked because the only math done is incrementing
@@ -48,16 +46,7 @@ abstract contract ERC1155Permit is ERC1155, EIP712 {
         unchecked {
             address recoveredAddress = ecrecover(
                 computeDigest(
-                    keccak256(
-                        abi.encode(
-                            PERMIT_TYPEHASH,
-                            owner,
-                            spender,
-                            id,
-                            nonces[owner][id]++,
-                            deadline
-                        )
-                    )
+                    keccak256(abi.encode(PERMIT_TYPEHASH, owner, spender, id, nonces[owner][id]++, deadline))
                 ),
                 v,
                 r,
--- src/tokens/ERC721/extensions/ERC721Permit.sol
+++ src/tokens/ERC721/extensions/ERC721Permit.sol
@@ -4,19 +4,18 @@ pragma solidity ^0.8.4;
 import {ERC721} from "../../../tokens/ERC721/ERC721.sol";
 import {EIP712} from "../../../utils/EIP712.sol";
 
-/// @notice ERC721 + EIP-2612-Style implementation.
+/// @notice ERC721 + EIP-2612-style implementation.
 /// @author SolDAO (https://github.com/Sol-DAO/solbase/blob/main/src/tokens/ERC721/extensions/ERC721Permit.sol)
 abstract contract ERC721Permit is ERC721, EIP712 {
+    /// -----------------------------------------------------------------------
+    /// EIP-2612-style Constants
+    /// -----------------------------------------------------------------------
 
-    /// -----------------------------------------------------------------------
-    /// EIP-2612-Style Constants
-    /// -----------------------------------------------------------------------
-    
-    // @dev 'keccak256("Permit(address owner,address spender,uint256 id,uint256 nonce,uint256 deadline)")'
+    /// @dev `keccak256("Permit(address owner,address spender,uint256 id,uint256 nonce,uint256 deadline)")`.
     bytes32 public constant PERMIT_TYPEHASH = 0x29da74a9365f97c3d77de334aec5c720e44b0c8a6e640ceb375e27a8ab7acadd;
 
     /// -----------------------------------------------------------------------
-    /// EIP-2612-Style Storage
+    /// EIP-2612-style Storage
     /// -----------------------------------------------------------------------
 
     mapping(uint256 => uint256) public nonces;
@@ -28,7 +27,7 @@ abstract contract ERC721Permit is ERC721, EIP712 {
     constructor(string memory _name, string memory _symbol) ERC721(_name, _symbol) EIP712(_name, "1") {}
 
     /// -----------------------------------------------------------------------
-    /// EIP-2612-Style Permit Logic
+    /// EIP-2612-style Permit Logic
     /// -----------------------------------------------------------------------
 
     function permit(
@@ -40,25 +39,13 @@ abstract contract ERC721Permit is ERC721, EIP712 {
         bytes32 r,
         bytes32 s
     ) public virtual {
-
         require(deadline >= block.timestamp, "PERMIT_DEADLINE_EXPIRED");
 
         // Unchecked because the only math done is incrementing
         // the owner's nonce which cannot realistically overflow.
         unchecked {
             address recoveredAddress = ecrecover(
-                computeDigest(
-                    keccak256(
-                        abi.encode(
-                            PERMIT_TYPEHASH,
-                            owner,
-                            spender,
-                            id,
-                            nonces[id]++,
-                            deadline
-                        )
-                    )
-                ),
+                computeDigest(keccak256(abi.encode(PERMIT_TYPEHASH, owner, spender, id, nonces[id]++, deadline))),
                 v,
                 r,
                 s
@@ -67,15 +54,15 @@ abstract contract ERC721Permit is ERC721, EIP712 {
             bool isApprovingAll = id == type(uint256).max;
 
             require(
-                (isApprovingAll || recoveredAddress == _ownerOf[id]) && 
-                recoveredAddress != address(0) && 
-                recoveredAddress == owner,
+                (isApprovingAll || recoveredAddress == _ownerOf[id]) &&
+                    recoveredAddress != address(0) &&
+                    recoveredAddress == owner,
                 "INVALID_SIGNER"
             );
 
             // If id is 2**256, then we assume the signer wants
             // to approve spender to spend all of their tokens.
-            if (isApprovingAll) {                
+            if (isApprovingAll) {
                 isApprovedForAll[recoveredAddress][spender] = true;
 
                 emit ApprovalForAll(recoveredAddress, spender, true);
