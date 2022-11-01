0942285de5f44551b23daf5f6f7df3f3d77f31de
z0r0z <92001561+z0r0z@users.noreply.github.com>
2022-11-01 20:45:55 +0900

    Update Owned.sol
    🥢  typographic nit

--- src/auth/Owned.sol
+++ src/auth/Owned.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.4;
 
 /// @notice Simple single owner authorization mixin.
-/// @author SolBase (https://github.com/Sol-DAO/solbase/blob/main/src/auth/Owned.sol)
+/// @author Solbase (https://github.com/Sol-DAO/solbase/blob/main/src/auth/Owned.sol)
 /// @author Modified from Solmate (https://github.com/transmissions11/solmate/blob/main/src/auth/Owned.sol)
 abstract contract Owned {
     /// -----------------------------------------------------------------------
