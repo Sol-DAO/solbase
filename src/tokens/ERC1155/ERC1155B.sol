80d8c2000cd1052c30b78e645eb4702cfd499ece
z0r0z <92001561+z0r0z@users.noreply.github.com>
2022-11-17 07:58:39 +0900

    ✨  Single supply token
    ✨  Single supply token

---
+++ src/tokens/ERC1155/ERC1155B.sol
@@ -0,0 +1,283 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+import {ERC1155TokenReceiver} from "./ERC1155.sol";
+
+/// @notice Modern, minimalist and gas-optimized ERC1155 implementation for single supply ids.
+/// @author Solbase (https://github.com/Sol-DAO/solbase/blob/main/src/tokens/ERC1155/ERC1155B.sol)
+/// @author Modified from Solmate (https://github.com/Rari-Capital/solmate/blob/main/src/tokens/ERC1155B.sol)
+abstract contract ERC1155B {
+    /// -----------------------------------------------------------------------
+    /// Events
+    /// -----------------------------------------------------------------------
+
+    event TransferSingle(
+        address indexed operator,
+        address indexed from,
+        address indexed to,
+        uint256 id,
+        uint256 amount
+    );
+
+    event TransferBatch(
+        address indexed operator,
+        address indexed from,
+        address indexed to,
+        uint256[] ids,
+        uint256[] amounts
+    );
+
+    event ApprovalForAll(address indexed owner, address indexed operator, bool approved);
+
+    event URI(string value, uint256 indexed id);
+
+    /// -----------------------------------------------------------------------
+    /// Custom Errors
+    /// -----------------------------------------------------------------------
+
+    error Unauthorized();
+
+    error WrongFrom();
+
+    error InvalidAmount();
+
+    error UnsafeRecipient();
+
+    error InvalidRecipient();
+
+    error LengthMismatch();
+
+    error AlreadyMinted();
+
+    error InvalidFrom();
+
+    error NotMinted();
+
+    /// -----------------------------------------------------------------------
+    /// ERC1155 Storage
+    /// -----------------------------------------------------------------------
+
+    mapping(address => mapping(address => bool)) public isApprovedForAll;
+
+    /// -----------------------------------------------------------------------
+    /// ERC1155B Storage
+    /// -----------------------------------------------------------------------
+
+    mapping(uint256 => address) public ownerOf;
+
+    function balanceOf(address owner, uint256 id) public view virtual returns (uint256 bal) {
+        address idOwner = ownerOf[id];
+
+        assembly {
+            // We avoid branching by using assembly to take
+            // the bool output of eq() and use it as a uint.
+            bal := eq(idOwner, owner)
+        }
+    }
+
+    /// -----------------------------------------------------------------------
+    /// Metadata Logic
+    /// -----------------------------------------------------------------------
+
+    function uri(uint256 id) public view virtual returns (string memory);
+
+    /// -----------------------------------------------------------------------
+    /// ERC1155 Logic
+    /// -----------------------------------------------------------------------
+
+    function setApprovalForAll(address operator, bool approved) public virtual {
+        isApprovedForAll[msg.sender][operator] = approved;
+
+        emit ApprovalForAll(msg.sender, operator, approved);
+    }
+
+    function safeTransferFrom(
+        address from,
+        address to,
+        uint256 id,
+        uint256 amount,
+        bytes calldata data
+    ) public virtual {
+        if (msg.sender != from)
+            if (!isApprovedForAll[from][msg.sender]) revert Unauthorized();
+
+        if (from != ownerOf[id]) revert WrongFrom(); // Can only transfer from the owner.
+
+        // Can only transfer 1 with ERC1155B.
+        if (amount != 1) revert InvalidAmount();
+
+        ownerOf[id] = to;
+
+        emit TransferSingle(msg.sender, from, to, id, amount);
+
+        if (to.code.length != 0) {
+            if (
+                ERC1155TokenReceiver(to).onERC1155Received(msg.sender, from, id, 1, data) !=
+                ERC1155TokenReceiver.onERC1155Received.selector
+            ) revert UnsafeRecipient();
+        } else if (to == address(0)) revert InvalidRecipient();
+    }
+
+    function safeBatchTransferFrom(
+        address from,
+        address to,
+        uint256[] calldata ids,
+        uint256[] calldata amounts,
+        bytes calldata data
+    ) public virtual {
+        if (ids.length != amounts.length) revert LengthMismatch();
+
+        if (msg.sender != from)
+            if (!isApprovedForAll[from][msg.sender]) revert Unauthorized();
+
+        // Storing these outside the loop saves ~15 gas per iteration.
+        uint256 id;
+        uint256 amount;
+
+        // Unchecked because the only math done is incrementing
+        // the array index counter which cannot possibly overflow.
+        unchecked {
+            for (uint256 i = 0; i < ids.length; i++) {
+                id = ids[i];
+                amount = amounts[i];
+
+                // Can only transfer from the owner.
+                if (from != ownerOf[id]) revert WrongFrom();
+
+                // Can only transfer 1 with ERC1155B.
+                if (amount != 1) revert InvalidAmount();
+
+                ownerOf[id] = to;
+            }
+        }
+
+        emit TransferBatch(msg.sender, from, to, ids, amounts);
+
+        if (to.code.length != 0) {
+            if (
+                ERC1155TokenReceiver(to).onERC1155BatchReceived(msg.sender, from, ids, amounts, data) !=
+                ERC1155TokenReceiver.onERC1155BatchReceived.selector
+            ) revert UnsafeRecipient();
+        } else if (to == address(0)) revert InvalidRecipient();
+    }
+
+    function balanceOfBatch(
+        address[] calldata owners,
+        uint256[] calldata ids
+    ) public view virtual returns (uint256[] memory balances) {
+        if (owners.length != ids.length) revert LengthMismatch();
+
+        balances = new uint256[](owners.length);
+
+        // Unchecked because the only math done is incrementing
+        // the array index counter which cannot possibly overflow.
+        unchecked {
+            for (uint256 i = 0; i < owners.length; ++i) {
+                balances[i] = balanceOf(owners[i], ids[i]);
+            }
+        }
+    }
+
+    /// -----------------------------------------------------------------------
+    /// ERC165 Logic
+    /// -----------------------------------------------------------------------
+
+    function supportsInterface(bytes4 interfaceId) public view virtual returns (bool) {
+        return
+            interfaceId == 0x01ffc9a7 || // ERC165 Interface ID for ERC165.
+            interfaceId == 0xd9b67a26 || // ERC165 Interface ID for ERC1155.
+            interfaceId == 0x0e89341c; // ERC165 Interface ID for ERC1155MetadataURI.
+    }
+
+    /// -----------------------------------------------------------------------
+    /// Internal Mint/Burn Logic
+    /// -----------------------------------------------------------------------
+
+    function _mint(address to, uint256 id, bytes memory data) internal virtual {
+        // Minting twice would effectively be a force transfer.
+        if (ownerOf[id] != address(0)) revert AlreadyMinted();
+
+        ownerOf[id] = to;
+
+        emit TransferSingle(msg.sender, address(0), to, id, 1);
+
+        if (to.code.length != 0) {
+            if (
+                ERC1155TokenReceiver(to).onERC1155Received(msg.sender, address(0), id, 1, data) !=
+                ERC1155TokenReceiver.onERC1155Received.selector
+            ) revert UnsafeRecipient();
+        } else if (to == address(0)) revert InvalidRecipient();
+    }
+
+    function _batchMint(address to, uint256[] memory ids, bytes memory data) internal virtual {
+        uint256 idsLength = ids.length; // Saves MLOADs.
+
+        // Generate an amounts array locally to use in the event below.
+        uint256[] memory amounts = new uint256[](idsLength);
+
+        uint256 id; // Storing outside the loop saves ~7 gas per iteration.
+
+        // Unchecked because the only math done is incrementing
+        // the array index counter which cannot possibly overflow.
+        unchecked {
+            for (uint256 i = 0; i < idsLength; ++i) {
+                id = ids[i];
+
+                // Minting twice would effectively be a force transfer.
+                if (ownerOf[id] != address(0)) revert AlreadyMinted();
+
+                ownerOf[id] = to;
+
+                amounts[i] = 1;
+            }
+        }
+
+        emit TransferBatch(msg.sender, address(0), to, ids, amounts);
+
+        if (to.code.length != 0) {
+            if (
+                ERC1155TokenReceiver(to).onERC1155BatchReceived(msg.sender, address(0), ids, amounts, data) !=
+                ERC1155TokenReceiver.onERC1155BatchReceived.selector
+            ) revert UnsafeRecipient();
+        } else if (to == address(0)) revert InvalidRecipient();
+    }
+
+    function _batchBurn(address from, uint256[] memory ids) internal virtual {
+        // Burning unminted tokens makes no sense.
+        if (from == address(0)) revert InvalidFrom();
+
+        uint256 idsLength = ids.length; // Saves MLOADs.
+
+        // Generate an amounts array locally to use in the event below.
+        uint256[] memory amounts = new uint256[](idsLength);
+
+        uint256 id; // Storing outside the loop saves ~7 gas per iteration.
+
+        // Unchecked because the only math done is incrementing
+        // the array index counter which cannot possibly overflow.
+        unchecked {
+            for (uint256 i = 0; i < idsLength; ++i) {
+                id = ids[i];
+
+                // Can only transfer from the owner.
+                if (from != ownerOf[id]) revert WrongFrom();
+
+                ownerOf[id] = address(0);
+
+                amounts[i] = 1;
+            }
+        }
+
+        emit TransferBatch(msg.sender, from, address(0), ids, amounts);
+    }
+
+    function _burn(uint256 id) internal virtual {
+        address owner = ownerOf[id];
+
+        if (owner == address(0)) revert NotMinted();
+
+        ownerOf[id] = address(0);
+
+        emit TransferSingle(msg.sender, owner, address(0), id, 1);
+    }
+}
